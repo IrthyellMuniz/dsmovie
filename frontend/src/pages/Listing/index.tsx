d04e491b02788227fe4c02bfa339bdbc6f421493
Irthyell Muniz <irthyellnaua@gmail.com>
2022-05-18 23:23:00 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,9 @@
-function Listing () {
+import Pagination from "components/Pagination";
 
-  return(
-    <h1>Página de Listagem</h1>
+function Listing() {
+
+  return (
+    <Pagination />
   );
 }
 
